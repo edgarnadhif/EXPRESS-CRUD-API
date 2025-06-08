63b1b75e62418b848c3dcb626d71aad454f2a500
edgarnadhif <edgarndf345@gmail.com>
2025-06-09 02:06:22 +0700

    ..

--- src/app.js
+++ src/app.js
@@ -1,23 +1,12 @@
-// import express from 'express';
-// import filmsRouter from './routes/films.js';
-
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// app.use(express.json());
-// app.use('/films', filmsRouter);
-
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server running at http://localhost:${PORT}`);
-// });
-
 import express from 'express';
 import filmsRouter from './routes/films.js';
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/films', filmsRouter);
 
-// Ekspor app untuk Vercel (tanpa app.listen)
-export default app;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
