42d0578f4f5c2d08d72b02659582500bfbde34ec
edgarnadhif <edgarndf345@gmail.com>
2025-06-09 07:38:13 +0700

    fix vercel

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
+// import express from 'express';
+// import filmsRouter from './routes/films.js';
+
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// app.use(express.json());
+// app.use('/films', filmsRouter);
+
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running at http://localhost:${PORT}`);
+// });
+
 import express from 'express';
 import filmsRouter from './routes/films.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+app.use(express.json()); // Untuk parsing body request
+app.use('/films', filmsRouter); // Gunakan router
 
-app.use(express.json());
-app.use('/films', filmsRouter);
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
-});
+export default app; // ⚠️ Ekspor sebagai handler
